6012692ed92349339db0aa3942abf5143909bd5d
Martin Komischke <martin.komischke@outlook.com>
2019-03-18 19:50:14 +0100

    update code

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Hello World');
+    response.send('Hello World!');
 });
 
 app.get('/api/courses', (request, response) => {
-    response.send([1, 2, 3]);
+    response.send([1, 2, 3, 4]);
 });
 
 app.listen(3000, () => {
