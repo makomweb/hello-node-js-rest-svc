e125851b21a6163469210cd896cff02c2cb36164
Martin Komischke <martin.komischke@outlook.com>
2019-03-18 19:38:15 +0100

    create an HTTP server

--- app.js
+++ app.js
@@ -7,4 +7,20 @@ app.get('/', (req, res) => {
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
-})
+});
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+
+    if (req.url === '/') {
+        res.write('Hello World!');
+        res.end();
+    }
+
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
+});
+
+server.listen(3001);
