e261cf4bee8eb9265d4e18c700753aff0d85415a
Martin Komischke <martin.komischke@outlook.com>
2019-03-18 19:57:59 +0100

    echo the courses id

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/api/courses', (request, response) => {
     response.send([1, 2, 3, 4]);
 });
 
+app.get('/api/courses/:id', (request, response) => {
+    response.send(request.params.id);
+});
+
 // PORT
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
